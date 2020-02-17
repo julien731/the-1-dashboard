d70851e6ef95f171b46f42015b10b10623e5ea1d
Julien Liabeuf <julien@liabeuf.fr>
2020-02-17 19:04:21 +0700

    Add registration form

---
+++ public/admin/partials/pages/register-user.php
@@ -0,0 +1,68 @@
+<div class="animated fadeIn">
+
+	<div class="row">
+
+		<div class="col-lg-6">
+			<div class="card">
+				<div class="card-header">
+					<strong>New Member</strong>
+				</div>
+				<div class="card-body card-block">
+					<form action="?page=view-user" method="post" enctype="multipart/form-data" class="form-horizontal">
+						<div class="row form-group">
+							<div class="col col-md-3"><label for="text-input" class=" form-control-label">Phone Prefix</label></div>
+							<div class="col-12 col-md-9"><input type="text" id="text-input" name="phone_prefix" placeholder="Text" class="form-control"></div>
+						</div>
+						<div class="row form-group">
+							<div class="col col-md-3"><label for="text-input" class=" form-control-label">Phone Number</label></div>
+							<div class="col-12 col-md-9"><input type="text" id="text-input" name="phone_number" placeholder="Text" class="form-control"></div>
+						</div>
+						<div class="row form-group">
+							<div class="col col-md-3"><label for="text-input" class=" form-control-label">First Name</label></div>
+							<div class="col-12 col-md-9"><input type="text" id="text-input" name="first_name" placeholder="Text" class="form-control"></div>
+						</div>
+						<div class="row form-group">
+							<div class="col col-md-3"><label for="text-input" class=" form-control-label">Last Name</label></div>
+							<div class="col-12 col-md-9"><input type="text" id="text-input" name="last_name" placeholder="Text" class="form-control"></div>
+						</div>
+						<div class="row form-group">
+							<div class="col col-md-3"><label for="text-input" class=" form-control-label">Date of Birth</label></div>
+							<div class="col-12 col-md-9"><input type="text" id="text-input" name="date_of_birth" placeholder="Text" class="form-control"></div>
+						</div>
+						<div class="row form-group">
+							<div class="col col-md-3"><label for="email-input" class=" form-control-label">Email</label></div>
+							<div class="col-12 col-md-9"><input type="email" id="email-input" name="email" placeholder="Enter Email" class="form-control"><small class="help-block form-text">Please enter your email</small></div>
+						</div>
+						<div class="row form-group">
+							<div class="col col-md-3"><label for="text-input" class=" form-control-label">PIN</label></div>
+							<div class="col-12 col-md-9"><input type="text" id="text-input" name="pin" placeholder="Text" class="form-control"></div>
+						</div>
+						<div class="row form-group">
+							<div class="col col-md-3"><label for="text-input" class=" form-control-label">Nationality ID</label></div>
+							<div class="col-12 col-md-9"><input type="text" id="text-input" name="nationality_id" placeholder="Text" class="form-control"></div>
+						</div>
+						<div class="row form-group">
+							<div class="col col-md-3"><label for="text-input" class=" form-control-label">Document Country Code</label></div>
+							<div class="col-12 col-md-9"><input type="text" id="text-input" name="document_country_code" placeholder="Text" class="form-control"></div>
+						</div>
+						<div class="row form-group">
+							<div class="col col-md-3"><label for="text-input" class=" form-control-label">Passport Number</label></div>
+							<div class="col-12 col-md-9"><input type="text" id="text-input" name="passport_number" placeholder="Text" class="form-control"></div>
+						</div>
+						<div class="row form-group">
+							<div class="col col-md-3"><label for="text-input" class=" form-control-label">Citizen ID</label></div>
+							<div class="col-12 col-md-9"><input type="text" id="text-input" name="citizen_id" placeholder="Text" class="form-control"></div>
+						</div>
+						<div class="form-actions form-group">
+							<input type="hidden" name="create_user" value="true">
+							<button type="submit" class="btn btn-primary btn-sm">Submit</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+
+</div>
