4ee26721599b889f6fdecbd8858195ebe31807d3
Julien Liabeuf <julien@liabeuf.fr>
2020-02-18 10:10:25 +0700

    Change title and favicon

--- public/admin/partials/header.php
+++ public/admin/partials/header.php
@@ -6,12 +6,12 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Ela Admin - HTML5 Admin Template</title>
-	<meta name="description" content="Ela Admin - HTML5 Admin Template">
+	<title>The 1 Testing Dashboard</title>
+	<meta name="description" content="The 1 Testing Dashboard">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="apple-touch-icon" href="https://i.imgur.com/QRAUqs9.png">
-	<link rel="shortcut icon" href="https://i.imgur.com/QRAUqs9.png">
+	<link rel="apple-touch-icon" href="https://www.the1.co.th/favicon.ico">
+	<link rel="shortcut icon" href="https://www.the1.co.th/favicon.ico">
 
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
